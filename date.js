06eb04249cb4bb93f3c3eb6dbe224b4d5372475e
Sébastien Duval <sebastienduval@live.com>
2023-05-18 08:22:55 -0400

    Support for numbers!
    Support for numbers!

---
+++ date.js
@@ -0,0 +1,10 @@
+function GenerateDate(date)
+{
+    const DaysOfWeek = ["sanda", "kizsanda", "nisda alokan", "nasda alokan", "iawda alokan", "skawategwikizgat", "kadaw sanda"];
+    const Months = ["alamikos", "pia8dagos", "mozokas", "sogalikas", "kikas", "nokahigas", "tmaskikos", "tmez8was", "skamonkas", "pnibagos", "mzatanos", "pbonkas"];
+
+    const DayOfWeek = DaysOfWeek[date.getDay()];
+    const Month = Months[date.getMonth()];
+
+    return DayOfWeek + ", " + "<Day Number>"/*Number.GenerateNumber(date.Day)*/ + " " + Month + ", " + "<Year Number>" /*Number.GenerateNumber(date.Year)*/ + " kasigaden.";
+}
---
+++ number.js
@@ -0,0 +1,70 @@
+function GenerateNumber(Number)
+{
+    const AnimateFigures = [ "ndakagwi", "pazgo", "niswak", "nlhoak", "iawak", "n8nnoak" ];
+    const InanimateFigures = [ "ndakagwi", "pazgwen", "nisnol","nhenol", "iawnol", "n8nnenol" ];
+    const Figures = [ "ndakagwi", "pazokw", "nis", "nas", "iaw", "n8lan", "ngwed8s", "t8baw8s", "ns8zek", "noliwi" ];
+    const Teens = [ "mdala", "ngwed8kaw", "nis8kaw", "nas8kaw", "iaw8kaw", "n8nn8kaw", "ngwed8s kas8kaw", "t8baw8s kas8kaw", "ns8zek kas8kaw", "noliwi kas8kaw" ];
+
+    const Tens = [ "nisinska", "nasinska", "iawinska", "n8nninska", "ngwed8s kasinska", "t8baw8s kasinska", "ns8zek kasinska", "noliwi kasinska" ];
+    const Hundreds = [ "ngwedatgwa", "nisatgwa", "nasatgwa", "iawatgwa", "n8nnatgwa", "ngwed8s kasatgwa", "t8baw8s kasatgwa", "ns8zek kasatgwa", "noliwi kasatgwa" ];
+    const Thousands = [ "ngwed8mkwaki", "nis8mkwaki", "nas8mkwaki", "iaw8mkwaki", "n8nn8mkwaki", "ngwed8s kas8mkwaki", "ngwed8s kas8mkwaki", "t8baw8s kas8mkwaki", "ns8zek kas8mkwaki", "noliwi kas8mkwaki" ];
+
+    let result = "";
+    if (Number < 10)
+    {
+        /*
+        // Exceptions for the first five figures.
+        if (type != Type.Neutral && Number != 0 && Number <= 5)
+        {
+            if ( type == Type.Animate )
+            {
+                result = AnimateFigures[Number];
+            }
+            else if ( type == Type.Inanimate )
+            {
+                result = InanimateFigures[Number];
+            }
+        }
+        else*/
+        {
+            result = Figures[Number];
+        }
+    }
+    else if (Number < 20)
+    {
+        result = Teens[Number - 10];
+    }
+    else if (Number < 100)
+    {
+        result += Tens[Math.floor(Number / 10) - 2];
+        if (Number % 10 > 0)
+        {
+            result += " taba " + GenerateNumber(Number % 10/*, Type.Neutral*/);
+        }
+    }
+    else if (Number < 1000)
+    {
+        result = Hundreds[Math.floor(Number / 100) - 1];
+        if (Number % 100 != 0)
+        {
+            result += " taba " + GenerateNumber(Number % 100/*, Type.Neutral*/);
+        }
+    }
+    else if (Number < 10000)
+    {
+        result = Thousands[Math.floor(Number / 1000) - 1];
+        if (Number % 1000 != 0)
+        {
+            result += " taba " + GenerateNumber(Number % 1000/*, Type.Neutral*/);
+        }
+    }
+    else if (Number < 1000000)
+    {
+        result = GenerateNumber(Math.floor(Number / 1000)) + " kas8mkwaki";
+        if (Number % 1000 != 0)
+        {
+            result += " taba " + GenerateNumber(Number % 1000/*, Type.Neutral*/);
+        }
+    }        
+    return result;
+}
