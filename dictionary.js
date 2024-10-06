b3a14446a9d7af3f64b4b469e7c5fdf9376bd535
Sébastien Duval <sebastienduval@live.com>
2024-10-06 09:15:34 -0400

    Copy to clipboard + Dict update.

--- dictionary.js
+++ dictionary.js
@@ -1,3 +1,4 @@
+
 function formatCategories(data)
 {
     var categoriesSelect = document.getElementById("categories");
@@ -10,28 +11,61 @@ function formatCategories(data)
     categoriesSelect.innerHTML = categoryInnerHTML;
 }
 
+function closureCopyEntryToClipboard(entry) 
+{ 
+    function copyEntryToClipboard()
+    {
+        if (navigator && navigator.clipboard && navigator.clipboard.writeText)
+        {
+            const str = entry.Abenaki + " [" + entry.Type + "] " + "(" + entry.French + ")" ;
+            return navigator.clipboard.writeText(str);
+        }
+        return Promise.reject('The Clipboard API is not available.');
+    };
+    return copyEntryToClipboard;
+}
+
 function formatToTable(data, category="", filter="", filter_language) {
-    var tableContainer = document.getElementById("sched-table");
+    var tableContainer = document.getElementById("word-table");
     var tableHTML = "<table>";
 
+    var tableEntries = [];
     for (var i = 0; i < data.length; i ++ ) 
     {
         const entry = data[i];
         if ( doesEntryIncludesCategory(entry, category) && 
             (!filter || containsIgnoreCase(entry[filter_language], filter)) )
-        {            
+        {         
+            const copyId = "copy" + tableEntries.length;
+            
             tableHTML += "<tr>";
             tableHTML += "<td>" + i + "</td>";
             tableHTML += "<td>" + entry.French + "</td>";
             tableHTML += "<td>" + entry.Type + "</td>";
             tableHTML += "<td>" + entry.Abenaki + "</td>";
             tableHTML += "<td>" + entry.Source + "</td>";
+            tableHTML += "<td id=" + copyId + "></td>";
             tableHTML += "</tr>";
+
+            tableEntries.push(entry);
         }
     }
 
     tableHTML += "</table>";
     tableContainer.innerHTML = tableHTML;
+
+    // Create the buttons for the cells.
+    for (var i = 0; i < tableEntries.length; i ++ ) 
+    {   
+        const copyId = "copy" + i;
+        var cell = document.getElementById(copyId); 
+        var btn = document.createElement("button");
+        btn.id = "button" + i;
+        const callback = closureCopyEntryToClipboard(tableEntries[i]);
+        btn.addEventListener("click", callback);
+        btn.innerHTML = "Copier";
+        cell.appendChild(btn);
+    }
 }
 
 function onCategoryChanged() 
