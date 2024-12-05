185623f4e42538c2f72fcf0191a3c571356d1f17
Sébastien Duval <sebastienduval@live.com>
2024-12-05 07:50:57 -0500

    Rename plurals to noun and update the algorithm.

--- plural.js
+++ plural.js
@@ -1,57 +1,113 @@
 const Pronouns = ["n", "k", "w", "n", "k", "k", "w"];
 
-function GenerateNoun(noun, animate, possessive, dependant)
+function GenerateNoun(noun, animate, plural, possessive, dependant)
 {
     // Trim the whitespaces.
     noun = noun.trim();
 
-    var plurals = [];
+    var nouns = [];
     if ( dependant )
     {
-        plurals = GenerateDependantPlural(noun, animate);
+        nouns = GenerateDependant(noun, animate, plural);
     }
     else
     {        
         if ( possessive )
         {
-            plurals = GeneratePossessive(noun, animate);
+            nouns = GeneratePossessive(noun, animate, plural);
         }
         else
         {
-            plurals.push(noun);
-        }
-
-        for (var i = 0; i < plurals.length; i ++ )
-        {
-            plurals[i] = GenerateIndependantPlural(plurals[i], animate);
+            if ( plural )
+            {
+                nouns.push(GenerateIndependantPlural(noun, animate));
+            }
+            else
+            {
+                nouns.push(noun);
+            }
         }
     }
-    return plurals;
+    return nouns;
 }
 
 // Todo: Manage the alienability.
-function GeneratePossessive(noun, animate)
+function GeneratePossessive(noun, animate, plural)
 {
+    function GeneratePronoun(noun, index)
+    {
+        if ( StartsWithVowel(noun) )
+        {
+            return Pronouns[index] + 'd';
+        }
+        return Pronouns[index];
+    }
+
+    function GenerateSingularPossive(noun)
+    {
+        if ( noun.endsWith("em") )
+        {
+            return noun + 'om';
+        }
+        if ( EndsWithVowel(noun) )
+        {
+            return noun + 'm';
+        }
+        if ( noun.endsWith('akw') || noun.endsWith("agw") 
+          || noun.endsWith("skw") || noun.endsWith("sgw") )
+        {
+            return noun.slice(0, -1) + "om";
+        }
+        return noun + 'em';
+    }
+
+    function GetPersonFinal(animate, index)
+    {
+        var finals;
+        if ( animate )
+        {
+            const animateFinals = ["", "", "a", "na", "na", "w8", "w8"];
+            finals = animateFinals;
+        }
+        else
+        {
+            const inanimateFinals = ["", "", "", "na", "na", "w8", "w8"];
+            finals = inanimateFinals;
+        }
+        return finals[index];
+    }
+
+    function GetPlural(animate, plural, index)
+    {
+        if ( !plural )
+        {
+            return '';
+        }
+        var finals;
+        if ( animate )
+        {
+            const animateFinals = ["ak", "ak", "", "wak", "wak", "k", ""];
+            finals = animateFinals;
+        }
+        else
+        {
+            const inanimateFinals = ["al", "al", "al", "wal", "wal", "l", "l"];
+            finals = inanimateFinals;
+        }
+        return finals[index];
+    }    
+    
+    noun = noun.toLowerCase();
+
     var result = [];
-    var finals;
-    if ( animate )
-    {
-        const Finals = ["", "", "a", "na", "na", "w8", "w8"];
-        finals = Finals;
-    }
-    else
-    {
-        const Finals = ["", "", "", "na", "na", "w8", "w8"];
-        finals = Finals;
-    }
     for ( var i = 0; i < Pronouns.length; i ++ )
     {
-        result.push(Pronouns[i]+noun+"em"+finals[i]);
+        result.push(GeneratePronoun(noun, i)+GenerateSingularPossive(noun)+GetPersonFinal(animate, i)+GetPlural(animate, plural, i));
     }
     return result;
 }
 
-function GenerateIndependantPlural(noun, animate=false)
+function GenerateIndependantPlural(noun, animate=false, possessive=false)
 {
     var plural = "";
     if (animate)
@@ -104,7 +160,7 @@ function GenerateIndependantPlural(noun, animate=false)
     return plural;
 }
 
-function GenerateDependantPlural(noun, animate=false)
+function GenerateDependant(noun, animate, plural)
 {
     var plurals = [];
     noun = noun.substring(1);
@@ -115,7 +171,7 @@ function GenerateDependantPlural(noun, animate=false)
         const Plurals = ["ak", "ak", "", "wak", "wak", "k", ""];
         for ( var i = 0; i < Pronouns.length; i ++ )
         {
-            plurals.push(Pronouns[i]+noun+Finals[i]+Plurals[i]);
+            plurals.push(Pronouns[i]+noun+Finals[i]+(plural?Plurals[i]:''));
         }
     }
     else
@@ -124,7 +180,7 @@ function GenerateDependantPlural(noun, animate=false)
         const Plurals = ["al", "al", "al", "wal", "wal", "al", "al"];
         for ( var i = 0; i < Pronouns.length; i ++ )
         {
-            plurals.push(Pronouns[i]+noun+Finals[i]+Plurals[i]);
+            plurals.push(Pronouns[i]+noun+Finals[i]+(plural?Plurals[i]:''));
         }
     }
     return plurals;
