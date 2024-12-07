6c2a009f65c0be0cf2f19bba9989caa81267271f
Sébastien Duval <sebastienduval@live.com>
2024-12-07 08:47:54 -0500

    Update locative page to support plural.

--- locative.js
+++ locative.js
@@ -1,24 +1,32 @@
-function GenerateLocative(noun)
+function GenerateLocative(noun, plurial)
 {
     // Trim the whitespaces.
     noun = noun.trim();
+    noun = noun.toLowerCase();
 
     var locative = "";
-    if (EndsWithVowel(noun))
+    if ( plurial )
     {
-        locative = noun + "k";
+        locative = noun + "ikok";
     }
-    else if ( noun.endsWith("em") || noun.endsWith("gen") )
-    {
-        locative = noun + "ok";
-    }
-    else if ( noun.endsWith("kw") || noun.endsWith("gw") )
-    {        
-        locative = noun.slice(0, -1) + "ok";
-    } 
     else
     {
-        locative = noun + "ek";
+        if (EndsWithVowel(noun))
+        {
+            locative = noun + "k";
+        }
+        else if ( noun.endsWith("em") || noun.endsWith("gen") )
+        {
+            locative = noun + "ok";
+        }
+        else if ( noun.endsWith("kw") || noun.endsWith("gw") )
+        {        
+            locative = noun.slice(0, -1) + "ok";
+        } 
+        else
+        {
+            locative = noun + "ek";
+        }
     }   
     return locative;
 }
