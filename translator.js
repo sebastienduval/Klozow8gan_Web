b52aca59f02de08be5e0f859b31481d09683a5ba
Sébastien Duval <sebastienduval@live.com>
2026-03-30 17:07:21 -0400

    Update translator.js

--- translator.js
+++ translator.js
@@ -1,7 +1,4 @@
-import { degeneratePlural } from "./modules/degenerator.js";
-import { degenerateLocative } from "./modules/degenerator.js";
-import { degeneratePossessive } from "./modules/degenerator.js";
-import { DegenerationContext } from "./modules/degenerator.js";
+import { extractPluralAffix, extractLocativeAffix, extractPossessiveAffix, DegenerationContext } from "./modules/affix_extractor.js";
 import { Dictionary } from "./modules/dictionary.js";
 
 function Translate()
@@ -19,27 +16,31 @@ function Translate()
         context.entry = dict.findEntry(Dictionary.ABENAKI, word);
         if ( context.entry == null )
         {
-            degeneratePlural(context);
-            console.log("Try degenerate plural (" + (context.entry != null? "success" : "failure") + ")");            
+            extractPluralAffix(context);
+            console.log("Try extract plural affix(" + (context.entry != null? "success" : "failure") + ")");            
         }
 
         if ( context.entry == null )
         {
-            degenerateLocative(context);             
-            console.log("Try degenerate locative (" + (context.entry != null? "success" : "failure") + ")");
+            extractLocativeAffix(context);             
+            console.log("Try extract locative affix (" + (context.entry != null? "success" : "failure") + ")");
         }
         
         if ( context.entry == null )
         {
-            degeneratePossessive(context);             
-            console.log("Try degenerate possessive (" + (context.entry != null? "success" : "failure") + ")");
+            extractPossessiveAffix(context);             
+            console.log("Try extract possessive affix (" + (context.entry != null? "success" : "failure") + ")");
         }            
 
         let resultMessage = "";
         if ( context.entry != null )
         {
             let temp = context.entry[Dictionary.FRENCH];
-            resultMessage = temp + (context.isLocative ? " (locatif)" : "") + (context.isPlural ? " (pluriel)" : "");
+            resultMessage = temp 
+            + (context.isLocative ? " (locatif)" : "") 
+            + (context.isPlural ? " (pluriel)" : "") 
+            + (context.isPossessive ? " (possessif)" : "") 
+            + (context.person ? " (personne " + context.person + ")" : "");
         }
         else
         {
